eddeb897e4b0756e6590a3500545fca80fe83419
Tara Bergener <tara.kelly211@gmail.com>
2025-03-08 12:30:30 -0500

    id matching for html error

--- utilities/index.js
+++ utilities/index.js
@@ -99,7 +99,7 @@ Util.buildManagementView = async function (req, res, next) {
 Util.buildClassificationList = async function (classification_id = null) {
   let data = await invModel.getClassifications()
   let classificationList =
-    '<select name="classification_id" id="classificationList" required>'
+    '<select name="classification_id" id="classificationSelect" required>'
   classificationList += "<option value=''>Choose a Classification</option>"
   data.rows.forEach((row) => {
     classificationList += '<option value="' + row.classification_id + '"'
