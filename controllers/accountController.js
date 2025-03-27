8dae0213ccbeab0a5eb8b090b2dec23b6c4caf6c
Tara Bergener <tara.kelly211@gmail.com>
2025-03-27 17:27:54 -0400

    hashed password update

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -219,9 +219,24 @@ async function editAccount(req, res) {
 async function editPassword(req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password, account_id } = req.body
+  
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the update.')
+    res.status(500).render("account/edit-password", {
+      title: "Edit Account",
+      nav,
+      errors: null,
+    })
+  }
+
   const editResult = await accountModel.updatePassword(
     account_id,
-    account_password,
+    hashedPassword,
   )
 
   if (editResult) {
